7d47e3c356e518cecf73c78645a219c54022b55f
Daniel Carradine <08131014@students.sunitafe.edu.au>
2023-09-13 05:32:46 +0000

    13/9/23

---
+++ work.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+n = int(input("enter a number: ")) 
+
+
+b = 1
+while b < n+1: 
+    print ("the number is "  +str(b)+ "its square is " +str(n) and "its cube is " +str(b) ) 
+    b += 1 
+
+
+
