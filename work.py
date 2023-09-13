afde0a5698efc77a9c03aa448864d1a9a9ef18ec
Daniel Carradine <08131014@students.sunitafe.edu.au>
2023-09-13 05:42:16 +0000

    13/9/23

--- work.py
+++ work.py
@@ -5,7 +5,7 @@ n = int(input("enter a number: "))
 
 b = 1
 while b < n+1: 
-    print ("the number is "  +str(b)+ "its square is " +str(n) and "its cube is " +str(b) ) 
+    print ("the number is "  +str(b)+ " its square is "  +str(b**2) +" and its cube is "  +str(b**3) ) 
     b += 1 
 
 
