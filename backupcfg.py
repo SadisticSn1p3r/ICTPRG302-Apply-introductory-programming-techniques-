96dad60701ac39d3b074b80e34b0c666302c33b6
Daniel Carradine <08131014@students.sunitafe.edu.au>
2023-09-14 05:32:35 +0000

    13/9/23

---
+++ backupcfg.py
@@ -0,0 +1,7 @@
+#/usr/bin/python3
+
+jobs = {"jobfile" : "/home/ec2-user/environment/ICTPRG302-Apply-introductory-programming-techniques/file1",
+       "jobdirectory" : "/home/ec2-user/environment/ICTPRG302-Apply-introductory-programming-techniques/dir1"} 
+       
+backupdir =  "/home/ec2-user/environment/ICTPRG302-Apply-introductory-programming-techniques-
+
--- work.py
+++ work.py
@@ -1,12 +1,30 @@
 #!/usr/bin/python3
 
-n = int(input("enter a number: ")) 
+import sys
 
-
-b = 1
-while b < n+1: 
-    print ("the number is "  +str(b)+ " its square is "  +str(b**2) +" and its cube is "  +str(b**3) ) 
-    b += 1 
+def main():
+    """
+    This Python code demonstrates the following features:
+    
+    * accessing command line arguments.
+    
+    """
+    try:
+        argCount = len(sys.argv)
+        program = sys.argv[0]
+        arg1 = sys.argv[1]
+        arg2 = sys.argv[2]
+        
+        print("The program name is " + program + ".")
+        print("The number of command line items is " + str(argCount) + ".")
+        print("Command line argument 1 is " + arg1 + ".")
+        print("Command line argument 2 is " + arg2 + ".")
+        
+    except:
+        print("ERROR: An error occurred.")
+    
+if __name__ == "__main__":
+    main()
 
 
 
